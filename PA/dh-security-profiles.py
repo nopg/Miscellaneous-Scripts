ee012d3e25ccadc5ee93f21e14d49c04f27c9289
Unknown <rgillespie@gmail.com>
2018-09-15 00:49:11 -0600

    PA scripts working

--- PA/dh-security-profiles.py
+++ PA/dh-security-profiles.py
@@ -2,24 +2,14 @@ import requests
 import getpass
 import sys
 import xmltodict, json
+import xml.etree.ElementTree as etree
+import io
 
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-def pp_json(json_thing, sort=True, indents=4):
-    """
-    PRETTY PRINT JSON WHETHER STRING OR DICTIONARY
-
-    :param json_thing: object to be printed
-    :return: None, print output
-    """
-    if type(json_thing) is str:
-        print("STR")
-        print(json.dumps(json.loads(json_thing), sort_keys=sort, indent=indents))
-    else:
-        print(json.dumps(json_thing, sort_keys=sort, indent=indents))
-    return None
+ANTIVIRUS = "/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name='vsys1']/profiles/virus"
 
 class rest_api_lib_pa:
     def __init__(self, pa_ip, username, password):
@@ -53,59 +43,71 @@ class rest_api_lib_pa:
         temp = xmltodict.parse(login_response.text)
         self.key = temp["response"]["result"]["key"]
 
-    def get_request_pa(self, mount_point, commands):
+    def get_request_pa(self, type="config", action="show", xpath=None, element=None):
         """GET request"""
 
-        command_list = commands.split()
-        
-        params = []
-        for command in command_list:
-            params.append("<" + command + ">")
-        for command in command_list[::-1]:
-            params.append("</" + command + ">")
-        
-        command = ''.join(params)
-
-        url = "https://{}:443/api{}{}&key={}".format(self.pa_ip, mount_point, command, self.key)
+        if not element:
+            url = f"https://{self.pa_ip}:443/api?type={type}&action={action}&xpath={xpath}&key={self.key}"
+        else:
+            url = f"https://{self.pa_ip}:443/api?type={type}&action={action}&xpath={xpath}&key={self.key}&element={element}"
         print(url)
         response = self.session[self.pa_ip].get(url, verify=False)
-        data = response.text
+        data = etree.fromstring(response.text)
         return data
 
-    def get_request_and_param_pa(self, mount_point, commands):
-        """GET request"""
+    # def get_request_pa_cli(self, mount_point, commands):
+    #     """GET request"""
 
-        command_list = commands.split()
-        value = command_list.pop()
-        params = []
-
-        for command in command_list:
-            params.append("<" + command + ">")
-        params.append(value)
-
-        for command in command_list[::-1]:
-            params.append("</" + command + ">")
+    #     command_list = commands.split()
         
-        command = ''.join(params)
+    #     params = []
+    #     for command in command_list:
+    #         params.append("<" + command + ">")
+    #     for command in command_list[::-1]:
+    #         params.append("</" + command + ">")
+        
+    #     command = ''.join(params)
 
-        url = "https://{}:443/api{}{}&key={}".format(self.pa_ip, mount_point, command, self.key)
-        print(url)
-        response = self.session[self.pa_ip].get(url, verify=False)
-        data = response.text
-        return data
+    #     url = "https://{}:443/api{}{}&key={}".format(self.pa_ip, mount_point, command, self.key)
+    #     print(url)
+    #     response = self.session[self.pa_ip].get(url, verify=False)
+    #     data = response.text
+    #     return data
 
-    def post_request(
-        self, mount_point, payload, headers={"Content-Type": "application/json"}
-    ):
-        """POST request"""
-        url = "https://{}:443/dataservice/{}".format(self.vmanage_ip, mount_point)
-        payload = json.dumps(payload)
-        response = self.session[self.vmanage_ip].post(
-            url=url, data=payload, headers=headers, verify=False
-        )
-        print(response)
-        data = response.text
-        return data
+    # def get_request_pa_cli_and_param(self, mount_point, commands):
+    #     """GET request"""
+
+    #     command_list = commands.split()
+    #     value = command_list.pop()
+    #     params = []
+
+    #     for command in command_list:
+    #         params.append("<" + command + ">")
+    #     params.append(value)
+
+    #     for command in command_list[::-1]:
+    #         params.append("</" + command + ">")
+        
+    #     command = ''.join(params)
+
+    #     url = "https://{}:443/api{}{}&key={}".format(self.pa_ip, mount_point, command, self.key)
+    #     print(url)
+    #     response = self.session[self.pa_ip].get(url, verify=False)
+    #     data = response.text
+    #     return data
+
+    # def post_request(
+    #     self, mount_point, payload, headers={"Content-Type": "application/json"}
+    # ):
+    #     """POST request"""
+    #     url = "https://{}:443/dataservice/{}".format(self.vmanage_ip, mount_point)
+    #     payload = json.dumps(payload)
+    #     response = self.session[self.vmanage_ip].post(
+    #         url=url, data=payload, headers=headers, verify=False
+    #     )
+    #     print(response)
+    #     data = response.text
+    #     return data
 
 
 if __name__ == "__main__":
@@ -120,15 +122,46 @@ if __name__ == "__main__":
     pa_ip = sys.argv[1]
     username = sys.argv[2]
     password = getpass.getpass("Enter Password: ")
-    command = input("What command would you like to run: ")
 
     obj = rest_api_lib_pa(pa_ip, username, password)
-    
-    test = obj.get_request_and_param_pa("?type=op&cmd=",command)
 
-    b = xmltodict.parse(test)
-    pp_json(b)
+    xpath = ANTIVIRUS #+ "/entry[@name='alert-only']"
+
+    av_objects = obj.get_request_pa(type='config',action='show',xpath=xpath)
+
+
+
+    with open("virus-alert-only.xml", "r") as fin:
+         av = fin.read()
+
+    entry_element = av
+
+    response = obj.get_request_pa(type="config",action="set",xpath=xpath,element=entry_element)
+
+    for elem in response.iter():
+         print(elem)
+         print(elem.attrib)
+         print(elem.text)
+
 
     print("\n\nComplete!\n")
 
 
+    #test = obj.get_request_pa_cli_and_param("?type=op&cmd=",command)
+
+    # def pp_xml(text):
+    #     b = xmltodict.parse(test)
+    #     pp_json(b)
+
+    #for elem in av_objects.iter():
+    #    print(elem)
+    # tree = etree.ElementTree(av_objects)
+    # e = av_objects.findall(".//result/virus/entry")
+    # for i in e:
+    #     print(i)
+    #     print(i.text)
+    #     print(i.attrib)
+
+    # data = etree.tostring(av_objects[0][0]).decode()
+    # with open ("xmltest.xml", "w") as fout:
+    #     fout.write(data)
