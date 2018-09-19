f652eefd77c467f5474d5a802801201ff336df4e
Unknown <rgillespie@gmail.com>
2018-09-19 13:42:38 -0600

    update debug to false

--- PaloAlto/rest_api_lib_pa.py
+++ PaloAlto/rest_api_lib_pa.py
@@ -46,7 +46,7 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-DEBUG = False
+DEBUG = True
 
 # REST API Class for use with Palo Alto API
 class rest_api_lib_pa:
