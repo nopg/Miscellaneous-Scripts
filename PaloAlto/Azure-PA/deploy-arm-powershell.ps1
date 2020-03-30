7b9d4dda6c2a8ee05d307b346f350e7711eb133c
Unknown <rgillespie@gmail.com>
2020-03-30 14:16:53 -0600

    just to deploy..

--- PaloAlto/Azure-PA/deploy-arm-powershell.ps1
+++ PaloAlto/Azure-PA/deploy-arm-powershell.ps1
@@ -2,6 +2,10 @@ New-AzResourceGroup -Name <resource-group-name> -Location <resource-group-locati
 New-AzResourceGroupDeployment -ResourceGroupName <resource-group-name> -TemplateUri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-vm-simple-linux/azuredeploy.json
 
 az ps:
-az group create --name <resource-group-name> --location <resource-group-location> #use this command when you need to create a new resource group for your deployment
-az group deployment create --resource-group <my-resource-group> --template-uri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-vm-simple-linux/azuredeploy.json
-az deployment group create --resource-group RG-Testing --template-uri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-vm-simple-linux/azuredeploy.json
+#create a new resource group for your deployment
+az group create --name <resource-group-name> --location <resource-group-location> 
+#deploy
+az deployment group create --resource-group <my-resource-group> --template-uri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-vm-simple-linux/azuredeploy.json
+
+#list Regions:
+az account list-locations
