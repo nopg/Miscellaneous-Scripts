8265a776a4e22b09d54207d799d16d4add0f910a
Ryan Gillespie <rgillespie@gmail.com>
2021-10-24 22:53:58 -0600

    cleanup

--- inventory_builder/inventory_builder.py
+++ inventory_builder/inventory_builder.py
@@ -26,54 +26,6 @@ def load_yaml(filename):
         sys.exit(0)
 
 
-## Stolen from: https://github.com/codazoda/tomark/blob/master/tomark/tomark.py (MIT Licensed)
-def to_markdown_table(listOfDicts):
-    """Loop through a list of dicts and return a markdown table as a multi-line string.
-    listOfDicts -- A list of dictionaries, each dict is a row
-    """
-    markdowntable = ""
-    # Make a string of all the keys in the first dict with pipes before after and between each key
-    markdownheader = '| ' + ' | '.join(map(str, listOfDicts[0].keys())) + ' |'
-    # Make a header separator line with dashes instead of key names
-    markdownheaderseparator = '|:-----' * len(listOfDicts[0].keys()) + '|'
-    # Add the header row and separator to the table
-    markdowntable += markdownheader + '\n'
-    markdowntable += markdownheaderseparator + '\n'
-    # Loop through the list of dictionaries outputting the rows
-    for row in listOfDicts:
-        markdownrow = ""
-        for key, col in row.items():
-            temp = str(col) ## Added by me, not in original
-            if '\n' in col:
-                temp = col.replace('\n',' |\n| | ')
-            markdownrow += '| ' + temp + ' '
-        markdowntable += markdownrow + '|' + '\n'
-    return markdowntable
-
-
-def build_csv(output, filename):
-    """
-    BUILD CSV BASED ON AN EXISTING DICTIONARY
-
-    :param output: existing dictionary to be written
-    :param filename: filename to create csv
-    :return:
-    """
-    
-    if output:
-        pass
-    else:
-        return None
-        
-    print("Building CSV...")
-    headers = list(output[0].keys())
-    fout = open(filename, 'w')
-    writer = csv.DictWriter(fout, fieldnames=headers, lineterminator='\n')
-    writer.writeheader()
-    writer.writerows(output)
-    fout.close()
-
-
 def build_device_list(filename):
     devices = load_yaml(filename)
     print(devices)
@@ -99,6 +51,52 @@ def build_device_list(filename):
     return IOSXE_DEVICES
 
 
+def to_markdown_table(listOfDicts):
+    """
+    Stolen with a tiny tweak, from: https://github.com/codazoda/tomark/blob/master/tomark/tomark.py (MIT Licensed)
+    Loop through a list of dicts and return a markdown table as a multi-line string.
+    listOfDicts -- A list of dictionaries, each dict is a row
+    """
+    if not listOfDicts:
+        return ""
+
+    markdowntable = ""
+    # Make a string of all the keys in the first dict with pipes before after and between each key
+    markdownheader = '| ' + ' | '.join(map(str, listOfDicts[0].keys())) + ' |'
+    # Make a header separator line with dashes instead of key names
+    markdownheaderseparator = '|:-----' * len(listOfDicts[0].keys()) + '|'
+    # Add the header row and separator to the table
+    markdowntable += markdownheader + '\n'
+    markdowntable += markdownheaderseparator + '\n'
+    # Loop through the list of dictionaries outputting the rows
+    for row in listOfDicts:
+        markdownrow = ""
+        for key, col in row.items():
+            temp = str(col)
+            if '\n' in col:
+                temp = col.replace('\n',' |\n| | ')
+            markdownrow += '| ' + temp + ' '
+        markdowntable += markdownrow + '|' + '\n'
+    return markdowntable
+
+
+def create_file(output, filename):
+    print(f"Building {filename}..")
+
+    with open(filename, 'w') as fout:
+        if filename.endswith(".csv"):
+            if not output:
+                print(f"FAILED {filename}.csv...no successes. Failures printed above (or check out markdown/html!)")
+                return None
+            headers = list(output[0].keys())
+            writer = csv.DictWriter(fout, fieldnames=headers, lineterminator='\n')
+            writer.writeheader()
+            writer.writerows(output)
+        else:
+            fout.write(output)
+
+
+# Below two to be converted to one function
 def format_sh_ver_results(sh_ver_results):
     dict_results = []
     failed = []
@@ -117,53 +115,90 @@ def format_sh_ver_results(sh_ver_results):
     return dict_results, failed
 
 
+# This and above to be converted to one function
 def format_manual_results(manual_results):
     dict_results = []
+    failed = []
 
     for result in manual_results:
-        temp = {}
-        temp["host"] = result.host
-        temp["result"] = str(result.result)
-        dict_results.append(temp)
+        if result.failed:
+            failed.append(result)
+        else:
+            temp = {}
+            temp["host"] = result.host
+            temp["result"] = result.result
+            dict_results.append(temp)
 
-    # print(dict_results)
-    # sys.exit(0)
-    return dict_results
+
+    return dict_results, failed
 
 
-def create_outputs(results, filename):
+def create_outputs(results, filename, failed):
+
+    # Add failure text if necessary
+    failure_text = ""
+    if failed:
+        failure_text = """\n\n\n
+        We failed to get output from the following devices:\n
+        Host\t\t\tError:\n
+        -----------------------------------------\n
+        """
+        for failure in failed:
+            failure_text += f"{failure.host}\t{failure.result}\n"
+
+        failure_text += "\n\n\n\n"
+        print(failure_text)
+
     # Create markdown
-    md_results = to_markdown_table(results)
+    md_results = failure_text
+    md_results_table = to_markdown_table(results)
+    
+    md_results += md_results_table
 
-    # Create html
-    html = "<style>table{border:3px solid black;}th, td{border:1px solid black;}</style>"
-    html += markdown.markdown(md_results, extensions=['tables'])
+    # Create html, style the lame html table
+    html = markdown.markdown(failure_text)
+    html += "<style>table{border:3px solid black;}th, td{border:1px solid black;}</style>"
+    html += markdown.markdown(md_results_table, extensions=['tables'])
 
     # Write Outputs
-    print("Building markdown")
-    with open(f"{filename}.md", 'w') as fin:
-        fin.write(md_results)
-    
-    print("Building html")
-    with open(f"{filename}.html", 'w') as fin:
-        fin.write(html)
+    create_file(md_results, f"{filename}.md")
+    create_file(html, f"{filename}.html")
+    create_file(results, f"{filename}.csv")
 
-    build_csv(results, f"{filename}.csv")
+
+def manual_command(username,password,IOSXE_DEVICES,command):
+
+    # Async get the outputs
+    manual_command_results = asyncio.get_event_loop().run_until_complete(async_show_command(username, password, IOSXE_DEVICES, command))
+
+    # Format for output dictionary / table
+    dict_results, failed = format_manual_results(manual_command_results)
+    
+    # Create whatever outputs
+    create_outputs(dict_results, "manual", failed)
+
+
+def create_inventory(username: str, password: str, IOSXE_DEVICES: dict) -> None: # Actually List(Dict)
+
+    sh_ver_results = asyncio.get_event_loop().run_until_complete(async_show_command(username, password, IOSXE_DEVICES, "show version"))
+
+    # Create useable output dictionary / table for ALL devices
+    dict_results, failed = format_sh_ver_results(sh_ver_results)
+
+    create_outputs(dict_results, "inventory", failed)
 
 
 async def get_show_command(device: dict, command: str) -> dict: # Actually List(Dict)---to update
     try:
         async with AsyncScrapli(**device) as conn:
-            version_response = await conn.send_command(command)
-        
-        return version_response
-
-    except Exception as e:
-        response = Response(device["host"],command)
-        response.result = e
+            response = await conn.send_command(command)
 
         return response
 
+    except Exception as e:
+        response = Response(device["host"],command)
+        response.result = str(e)
+        return response
 
 
 async def async_show_command(username: str, password: str, devices: dict, command: str):
@@ -175,86 +210,15 @@ async def async_show_command(username: str, password: str, devices: dict, comman
     return sh_command_results
 
 
-def manual_command(username,password,filename,command):
-    IOSXE_DEVICES = build_device_list(filename)
-
-    manual_command_results = asyncio.get_event_loop().run_until_complete(async_show_command(username, password, IOSXE_DEVICES, command))
-
-    for result in manual_command_results:
-        print(result.host)
-        print(result.result)
-
-
-    # Create useable output dictionary / table for ALL devices
-    dict_results = format_manual_results(manual_command_results)
-
-    create_outputs(dict_results, "manual")
-
-
-
-def create_inventory(username: str, password: str, filename: str) -> None:
-    
-    IOSXE_DEVICES = build_device_list(filename)
-
-    sh_ver_results = asyncio.get_event_loop().run_until_complete(async_show_command(username, password, IOSXE_DEVICES, "show version"))
-
-    print("Output: ")
-
-    # Create useable output dictionary / table for ALL devices
-    dict_results, failed = format_sh_ver_results(sh_ver_results)
-
-    # Create markdown
-    md_results = to_markdown_table(dict_results)
-
-    # Create html
-    html = "<style>table{border:3px solid black;}th, td{border:1px solid black;}</style>"
-    html += markdown.markdown(md_results, extensions=['tables'])
-
-    # Add failure text if necessary
-    if failed:
-        failure_text = """\n\n
-        We failed to get output from the following devices:\n
-        Host\t\t\tError:\n
-        -----------------------------------------\n
-        """
-        for failure in failed:
-            failure_text += f"{failure.host}\t{failure.result}\n"
-        failure_text += "\n"
-
-        md_results += failure_text
-        html += markdown.markdown(failure_text)
-
-        print(failure_text)
-
-    # Write Outputs
-    print("Building markdown")
-    with open(f"test.md", 'w') as fin:
-        fin.write(md_results)
-    
-    print("Building html")
-    with open(f"test.html", 'w') as fin:
-        fin.write(html)
-
-    build_csv(dict_results, "test.csv")
-
-
-# def load_manual(username: str, password: str, filename: str) -> None:
-#     asyncio.get_event_loop().run_until_complete(main(username, password, filename))
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Please use this syntax:")
     parser.add_argument("-u", "--username", help="Username for Cisco Devices", type=str)
     parser.add_argument("-p", "--password", help="Password for Cisco Devices", type=str)
     parser.add_argument("-f", "--filename", help="Device List File (YAML)", type=str)
-    parser.add_argument("-d", "--directory", help="Directory containing 'show version' output.", type=str)
+    #parser.add_argument("-d", "--directory", help="Directory containing 'show version' output.", type=str)
     parser.add_argument("-i", "--inventory", help="Create a new inventory only.(any value)", type=str)
     parser.add_argument("-c", "--command", help="Send this command", type=str)
     args = parser.parse_args()
-
-    # if args.directory:
-    #     load_manual()
-    #     sys.exit(0)
-
     username = args.username
     password = args.password
     filename = args.filename
@@ -267,10 +231,12 @@ if __name__ == "__main__":
     if not filename:
         filename = input("Device List File: ")
 
+    IOSXE_DEVICES = build_device_list(filename)
+
     if args.inventory:
-        create_inventory(username,password,filename)
+        create_inventory(username,password,IOSXE_DEVICES)
     
     if command:
-        manual_command(username, password, filename, command)
+        manual_command(username, password, IOSXE_DEVICES, command)
 
-    print("Done.")
+    print("\nDone.\n")
